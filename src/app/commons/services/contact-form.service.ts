639fa751a6508db9aba7343f5dcfea538740e072
Braynner Polo Pichón <brapo0298@gmail.com>
2025-04-12 20:59:26 -0500

    apiUrl railway updated

--- src/app/commons/services/contact-form.service.ts
+++ src/app/commons/services/contact-form.service.ts
@@ -7,7 +7,9 @@ import { ContactFormResponse } from '../interfaces/response.interface';
   providedIn: 'root',
 })
 export class ContactFormService {
-  private apiUrl = 'http://localhost:3023';
+  private apiUrl = 'https://personallandingbackend-production.up.railway.app/';
+  // private apiUrl = 'http://localhost:3023';
+
 
   constructor(private httpClient: HttpClient) {}
 
