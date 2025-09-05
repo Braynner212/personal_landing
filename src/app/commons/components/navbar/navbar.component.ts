8deb734dd8700dff70d92fa3c60c9209fbace705
Braynner Polo Pichón <brapo0298@gmail.com>
2025-09-05 03:03:17 -0500

    menu hamburger icon and system Menu removed - home phrase and description fixed

--- src/app/commons/components/navbar/navbar.component.ts
+++ src/app/commons/components/navbar/navbar.component.ts
@@ -1,38 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { MenuItemService } from '../../services/menu-item.service';
 
 @Component({
   standalone: true,
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule],
 })
-export class NavbarComponent implements OnInit {
-  currentMenuItem = 'home';
-  menuOptions: { content: string; name: string, href: string }[] = [
-    { content: 'Inicio', name: 'home', href: '#home', },
-    { content: 'Nosotros', name: 'about', href: '#about', },
-    { content: 'servicios', name: 'solutions', href: '#solution' },
-/*     { content: 'Habilidades', name: 'skills', href: '#skills' },
-    { content: 'Experiencia', name: 'experience', href: '#experience' }, */
-    { content: 'Convercemos', name: 'contact', href: '#contact' },
-  ];
-
-  constructor( private menuItemServ: MenuItemService) {}
-  
-  ngOnInit(): void {
-    this.menuItemServ.scrollTo('home');
-
-    this.menuItemServ.currentMenuItem$.subscribe((elementId) => {
-      this.currentMenuItem = elementId;
-    });
-  }
-
-  scrollTo(elementId: string) {
-    this.menuItemServ.scrollTo(elementId);
-  }
-
-}
+export class NavbarComponent {}
